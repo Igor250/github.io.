9bd130900a9fe11fa506f06f62c5775090180940
Igor250 <31507254+Igor250@users.noreply.github.com>
2017-09-10 19:53:49 +0300

    9

--- send.php
+++ send.php
@@ -1,6 +1,17 @@
-<?
-$to = "sn2879@i.ua";
-mail($to,"$sub",$mes);
-echo "Ваше сообщение с темой  $sub и текстом: $mes <br> отправлено<BR>;
-?>
-
+<?php
+$fio = $_POST['fio'];
+$email = $_POST['email'];
+$fio = htmlspecialchars($fio);
+$email = htmlspecialchars($email);
+$fio = urldecode($fio);
+$email = urldecode($email);
+$fio = trim($fio);
+$email = trim($email);
+//echo $fio;
+//echo "<br>";
+//echo $email;
+if (mail("sn2879@i.ua", "Заявка с сайта", "ФИО:".$fio.". E-mail: ".$email ,"From: sn28792@i.ua \r\n"))
+ {     echo "сообщение успешно отправлено"; 
+} else { 
+    echo "при отправке сообщения возникли ошибки";
+}?>
