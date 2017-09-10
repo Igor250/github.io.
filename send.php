dc15ba387fb21cb36e5d718ed3a017ccb13cbb6b
Igor250 <31507254+Igor250@users.noreply.github.com>
2017-09-10 19:28:57 +0300

    6

--- send.php
+++ send.php
@@ -1,6 +1,6 @@
 <?
 $to = "_@_";
 mail($to,"$sub",$mes);
-echo "Ваше сообщение с темой  $sub и текстом: $mes <br> отправлено<BR><center><a href='http://www.svoisait.ru/form/_post.shtml'>Отправить ещё</a>";
+echo "Ваше сообщение с темой  $sub и текстом: $mes <br> отправлено<BR><center><a href='http://www.dmdz.ga/form/_post.shtml'>Отправить ещё</a>";
 ?>
 
