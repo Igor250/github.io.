1cec1441e1423eabc1b319c674c5214f703c7f77
Igor250 <31507254+Igor250@users.noreply.github.com>
2017-09-10 19:25:46 +0300

    5

---
+++ send.php
@@ -0,0 +1,6 @@
+<?
+$to = "_@_";
+mail($to,"$sub",$mes);
+echo "Ваше сообщение с темой  $sub и текстом: $mes <br> отправлено<BR><center><a href='http://www.svoisait.ru/form/_post.shtml'>Отправить ещё</a>";
+?>
+
